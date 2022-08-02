8eefa8b2cf02b85d3f13175ed8992f1fa779afe0
saidctb <saidctb@live.fr>
2022-08-02 05:00:56 +0200

    fix bug

--- post_processing.py
+++ post_processing.py
@@ -7,10 +7,10 @@ degrees  = [2,3,4,5]
 number_of_mpi_procs = [1*32,2*32,4*32,8*32,16*32,32*32,64*32,128*32]
 number_of_threads = 1
 
-timmings_bi_assembly    = np.zeros((len(problems), len(ncells),len(degrees), len(number_of_mpi_procs)))
-timmings_dot_p_assembly = np.zeros((len(problems), len(ncells),len(degrees), len(number_of_mpi_procs)))
-scaling_bi_assembly     = timmings_bi_assembly.copy()
-scaling_dot_p_assembly  = timmings_dot_p_assembly.copy()
+timmings_bi_assembly   = np.zeros((len(problems), len(ncells),len(degrees), len(number_of_mpi_procs)))
+timmings_dot_p         = np.zeros((len(problems), len(ncells),len(degrees), len(number_of_mpi_procs)))
+scaling_bi_assembly    = timmings_bi_assembly.copy()
+scaling_dot_p          = timmings_dot_p.copy()
 
 for i1,p in enumerate(problems):
     for i2,nc in enumerate(ncells):
@@ -21,21 +21,21 @@ for i1,p in enumerate(problems):
                 try:
                     T = np.load(results_folder+filename+'.npy', allow_pickle=True)
                     T = T.item()
-                    timmings_bi_assembly[i1,i2,i3,i4] = T['dot_product_time']
-                    timmings_dot_p_assembly[i1,i2,i3,i4] = T['bilinear_form_assembly_time']
+                    timmings_bi_assembly[i1,i2,i3,i4] = T['bilinear_form_assembly_time']
+                    timmings_dot_p[i1,i2,i3,i4]       = T['dot_product_time']
                 except:
                     timmings_bi_assembly[i1,i2,i3,i4] = np.nan
-                    timmings_dot_p_assembly[i1,i2,i3,i4] = np.nan
+                    timmings_dot_p[i1,i2,i3,i4] = np.nan
 
             k = [j for j in range(len(number_of_mpi_procs)) if not np.isnan(timmings_bi_assembly[i1,i2,i3,j])] + [0]
-            scaling_bi_assembly[i1,i2,i3,:]    = timmings_bi_assembly[i1,i2,i3,:]/timmings_bi_assembly[i1,i2,i3,k[0]]
-            scaling_dot_p_assembly[i1,i2,i3,:] = timmings_dot_p_assembly[i1,i2,i3,:]/timmings_dot_p_assembly[i1,i2,i3,k[0]]
+            scaling_bi_assembly[i1,i2,i3,:] = timmings_bi_assembly[i1,i2,i3,:]/timmings_bi_assembly[i1,i2,i3,k[0]]
+            scaling_dot_p[i1,i2,i3,:]       = timmings_dot_p[i1,i2,i3,:]/timmings_dot_p[i1,i2,i3,k[0]]
 
 from tabulate import tabulate
 headers = [""] + [str(np) for np in number_of_mpi_procs]
 
 for i1,p in enumerate(problems):
-    print("="*45,"Timings of the Matrix Assembly  of {}".format(p), "="*45)
+    print("="*45,"Timings of the Matrix Assembly of {}".format(p), "="*45)
     T = np.around(timmings_bi_assembly[i1], decimals=5).reshape((len(ncells)*len(degrees), len(number_of_mpi_procs))).tolist()
     newT = []
     for i2,nc in enumerate(ncells):

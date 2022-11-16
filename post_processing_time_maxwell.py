702b074ff41d5d6ed83904b229e68872bc553cf2
saidctb <saidctb@live.fr>
2022-11-16 07:25:48 +0100

    add time integrator timmings

--- post_processing_time_maxwell.py
+++ post_processing_time_maxwell.py
@@ -7,10 +7,12 @@ number_of_mpi_procs = [1*35,2*35,4*35,8*35,16*35,32*35,64*35,128*35]
 number_of_threads   = 1
 filename            = 'maxwell3d'
 
-timmings_bi_assembly = np.zeros((len(ncells),len(degrees), len(number_of_mpi_procs)))
-timmings_dot_p       = np.zeros((len(ncells),len(degrees), len(number_of_mpi_procs)))
-scaling_bi_assembly  = timmings_bi_assembly.copy()
-scaling_dot_p        = timmings_dot_p.copy()
+timmings_bi_assembly     = np.zeros((len(ncells),len(degrees), len(number_of_mpi_procs)))
+timmings_time_integrator = np.zeros((len(ncells),len(degrees), len(number_of_mpi_procs)))
+timmings_dot_p           = np.zeros((len(ncells),len(degrees), len(number_of_mpi_procs)))
+scaling_bi_assembly      = timmings_bi_assembly.copy()
+scaling_time_integrator  = timmings_time_integrator.copy()
+scaling_dot_p            = timmings_dot_p.copy()
 
 for i1,nc in enumerate(ncells):
     for i2,d in enumerate(degrees):
@@ -20,15 +22,18 @@ for i1,nc in enumerate(ncells):
             try:
                 T = np.load(results_folder+filename+'.npy', allow_pickle=True)
                 T = T.item()
-                timmings_bi_assembly[i1,i2,i3] = T['bilinear_form_assembly_time']
-                timmings_dot_p[i1,i2,i3]       = T['dot_product_time']
+                timmings_bi_assembly[i1,i2,i3]     = T['bilinear_form_assembly_time']
+                timmings_time_integrator[i1,i2,i3] = T['solution']
+                timmings_dot_p[i1,i2,i3]           = T['dot_product_time']
             except:
                 timmings_bi_assembly[i1,i2,i3] = np.nan
+                timmings_time_integrator[i1,i2,i3] = np.nan
                 timmings_dot_p[i1,i2,i3] = np.nan
 
         k = [j for j in range(len(number_of_mpi_procs)) if not np.isnan(timmings_bi_assembly[i1,i2,j])] + [0]
-        scaling_bi_assembly[i1,i2,:] = timmings_bi_assembly[i1,i2,:]/timmings_bi_assembly[i1,i2,k[0]]
-        scaling_dot_p[i1,i2,:]       = timmings_dot_p[i1,i2,:]/timmings_dot_p[i1,i2,k[0]]
+        scaling_bi_assembly[i1,i2,:]     = timmings_bi_assembly[i1,i2,:]/timmings_bi_assembly[i1,i2,k[0]]
+        scaling_time_integrator[i1,i2,:] = timmings_time_integrator[i1,i2,:]/timmings_bi_assembly[i1,i2,k[0]]
+        scaling_dot_p[i1,i2,:]           = timmings_dot_p[i1,i2,:]/timmings_dot_p[i1,i2,k[0]]
 
 #-----------------------------------------------------------------------------------------------------------------------
 from tabulate import tabulate
@@ -45,9 +50,21 @@ if not all(np.isnan(v) for v in timmings_bi_assembly.flatten()):
 
     print(tabulate(newT, headers=headers, tablefmt="grid"))
     print("\n")
-    
+
+if not all(np.isnan(v) for v in timmings_time_integrator.flatten()):
+    print("="*45,"Timings of the time intergrator of Time dependent Maxwell", "="*45)
+    T = np.around(timmings_time_integrator, decimals=5)
+    newT = []
+    for i1,nc in enumerate(ncells):
+        for i2,d in enumerate(degrees):
+            newT.append(["nc = {} ** 3 , p = {}".format(nc,d)] +  T[i1,i2].tolist())
+        newT.append(["   "]*len(T[0]))
+
+    print(tabulate(newT, headers=headers, tablefmt="grid"))
+    print("\n")
+
 if not all(np.isnan(v) for v in timmings_dot_p.flatten()):
-    print("="*45,"Timings of the Matrix vector dot product of the Hcurl Mass Matrix", "="*45)
+    print("="*45,"Timings of the Matrix vector product of the Hcurl Mass Matrix", "="*45)
     T = np.around(timmings_dot_p, decimals=5)
     newT = []
     for i1,nc in enumerate(ncells):

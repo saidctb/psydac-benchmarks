f0389814efc830b5ecd559faa0baf813966116b7
saidctb <saidctb@live.fr>
2022-11-16 08:59:05 +0100

    update batch scripts for time maxwell

--- run_jobs_time_maxwell.py
+++ run_jobs_time_maxwell.py
@@ -32,22 +32,22 @@ export OMPI_MCA_mpi_yield_when_idle=1
 
 nnodes          = [1,2,2**2,2**3,2**4,2**5,2**6,2**7]
 ntasks_per_node = 35
-ncells          = [50,75,100,150,200]
+ncells          = [50,75,100,125,150,175,200]
 degrees         = [2,3]
 
 script_nc_d = 'srun python3 {filename}.py -n {nc} {nc} {nc} -d {d} {d} {d}\n'
 
-for f in filenames:
-    for nn in nnodes:
-        batch_script = batch_str.format(filename=f, nprocs=nn*ntasks_per_node,nnodes=nn,ntasks_per_node=ntasks_per_node, mem=150000,time_limit="6:00:00")
-        for nc in ncells:
-            for d in degrees:
-                batch_script += script_nc_d.format(filename=f, nc=nc, d=d)
+f = 'maxwell3d'
+for nn in nnodes:
+    batch_script = batch_str.format(filename=f, nprocs=nn*ntasks_per_node,nnodes=nn,ntasks_per_node=ntasks_per_node, mem=150000,time_limit="6:00:00")
+    for nc in ncells:
+        for d in degrees:
+            batch_script += script_nc_d.format(filename=f, nc=nc, d=d)
 
-            batch_script += '\n'
+        batch_script += '\n'
 
-        filename = 'job_{filename}_{nprocs}.sh'.format(filename=f, nprocs=nn*ntasks_per_node)
-        with open(filename,'w') as file_:
-            file_.write(batch_script)
+    filename = 'job_{filename}_{nprocs}.sh'.format(filename=f, nprocs=nn*ntasks_per_node)
+    with open(filename,'w') as file_:
+        file_.write(batch_script)
 
-        os.system('sbatch {}'.format(filename))
+    os.system('sbatch {}'.format(filename))

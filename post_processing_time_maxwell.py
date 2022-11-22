8dfb10c481244ece7504c1fb1ccca594916ec9c2
saidctb <saidctb@live.fr>
2022-11-22 10:12:19 +0100

    change ncells

--- post_processing_time_maxwell.py
+++ post_processing_time_maxwell.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 results_folder = 'results/'
-ncells   = [50,75,100,125,150, 175,200]
+ncells   = [125,150,175]
 degrees  = [2,3]
 number_of_mpi_procs = [1*35,2*35,4*35,8*35,16*35,32*35,64*35,128*35]
 number_of_threads   = 1
@@ -31,9 +31,10 @@ for i1,nc in enumerate(ncells):
                 timmings_dot_p[i1,i2,i3] = np.nan
 
         k = [j for j in range(len(number_of_mpi_procs)) if not np.isnan(timmings_bi_assembly[i1,i2,j])] + [0]
-        scaling_bi_assembly[i1,i2,:]     = timmings_bi_assembly[i1,i2,:]/timmings_bi_assembly[i1,i2,k[0]]
-        scaling_time_integrator[i1,i2,:] = timmings_time_integrator[i1,i2,:]/timmings_bi_assembly[i1,i2,k[0]]
-        scaling_dot_p[i1,i2,:]           = timmings_dot_p[i1,i2,:]/timmings_dot_p[i1,i2,k[0]]
+        nn = [np.nan]*k[0] + [2**(i-k[0]) for i in range(k[0],len(number_of_mpi_procs))]
+        scaling_bi_assembly[i1,i2,:]     = timmings_bi_assembly[i1,i2,k[0]]/timmings_bi_assembly[i1,i2,:]/nn
+        scaling_time_integrator[i1,i2,:] = timmings_time_integrator[i1,i2,k[0]]/timmings_time_integrator[i1,i2,:]/nn
+        scaling_dot_p[i1,i2,:]           = timmings_dot_p[i1,i2,k[0]]/timmings_dot_p[i1,i2,:]/nn
 
 #-----------------------------------------------------------------------------------------------------------------------
 from tabulate import tabulate
@@ -41,7 +42,7 @@ headers = [""] + [str(np) for np in number_of_mpi_procs]
 
 if not all(np.isnan(v) for v in timmings_bi_assembly.flatten()):
     print("="*45,"Timings of the Matrix Assembly of Time dependent Maxwell", "="*45)
-    T = np.around(timmings_bi_assembly, decimals=5)
+    T = np.around(scaling_bi_assembly, decimals=5)
     newT = []
     for i1,nc in enumerate(ncells):
         for i2,d in enumerate(degrees):
@@ -53,7 +54,7 @@ if not all(np.isnan(v) for v in timmings_bi_assembly.flatten()):
 
 if not all(np.isnan(v) for v in timmings_time_integrator.flatten()):
     print("="*45,"Timings of the time intergrator of Time dependent Maxwell", "="*45)
-    T = np.around(timmings_time_integrator, decimals=5)
+    T = np.around(scaling_time_integrator, decimals=5)
     newT = []
     for i1,nc in enumerate(ncells):
         for i2,d in enumerate(degrees):
@@ -65,7 +66,7 @@ if not all(np.isnan(v) for v in timmings_time_integrator.flatten()):
 
 if not all(np.isnan(v) for v in timmings_dot_p.flatten()):
     print("="*45,"Timings of the Matrix vector product of the Hcurl Mass Matrix", "="*45)
-    T = np.around(timmings_dot_p, decimals=5)
+    T = np.around(scaling_dot_p, decimals=5)
     newT = []
     for i1,nc in enumerate(ncells):
         for i2,d in enumerate(degrees):
@@ -74,4 +75,46 @@ if not all(np.isnan(v) for v in timmings_dot_p.flatten()):
 
     print(tabulate(newT, headers=headers, tablefmt="grid"))
     print("\n")
-            
+
+#====================================================================================================
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+from matplotlib.legend_handler import HandlerLine2D
+
+from itertools import product
+
+colors = cm.rainbow(np.linspace(0, 1, len(degrees)*len(ncells)).reshape((len(ncells),len(degrees))))
+titles = ['Matrix Assembly']
+names  = ['assembly']
+timings = timmings_bi_assembly
+for title,name in zip(titles, names):
+    fig = plt.figure()
+#    fig.suptitle(title)
+    for nc in range(len(ncells)):
+        ax = fig.add_subplot(2, 2, nc+1)
+        for p in range(2,4):
+            row = '$p={}$'.format(p)
+            line, = ax.plot(number_of_mpi_procs, timings[nc,p-2], 's-',color=colors[nc, p-2], label='Psydac,' + row)
+            #ax.plot(number_of_mpi_procs,timings[nc,p-2],'s', color=colors[nc,p-2])
+
+        ax.plot(number_of_mpi_procs,[0.3*np.nanmax(timings[0,p-2])/2**d for d in range(len(number_of_mpi_procs))], color='black', linestyle='dashed', label='perfect scaling')
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+        ax.legend(handler_map={line: HandlerLine2D(numpoints=4)})
+        # Put a legend to the right of the current axis
+        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        #ax.legend(bbox_to_anchor=(1, 1), loc='upper right', ncol=1, fontsize=7)
+        ax.set_xlabel( r'nprocs', rotation='horizontal' )
+        ax.set_ylabel( r'time [s]' )
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        ax.set_xticks([])
+        ax.set_xticks(number_of_mpi_procs)
+        ax.set_xticklabels([str(d) for d in number_of_mpi_procs])
+        ax.grid(True)
+        ax.title.set_text('$ncells={}^3$'.format(ncells[nc]))
+    fig.subplots_adjust(hspace = .4, wspace=.4, left=0.05, right=0.9)
+    #fig.tight_layout()
+plt.show()

ab0df2ceeb8369255340548cb42a18bd2b14118e
said <said-ctb@live.fr>
2023-01-10 22:25:47 +0100

    improve post processing

--- post_processing_sn.py
+++ post_processing_sn.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 results_folder = 'results/'
-#problems = ['poisson_3d','time_harmonic_maxwell_3d','biharmonic_3d']
+problems = ['poisson_3d','time_harmonic_maxwell_3d','biharmonic_3d']
 problems = ['biharmonic_3d']
-#mappings = [[['identity', True],['identity', False]],[['identity', True]],[['identity', False]]]
-mappings = [[['quarter_annulus', False]]]
-ncells   = [128,160]
+mappings = [[['identity', True],['identity', False]],[['identity', True]],[['identity', False]]]
+mappings = [[['identity', False]]]
+ncells   = [32,40]
 degrees  = [2,3,4]
-number_of_nodes     = np.array([1,2,4,8,16,32,64])
-number_of_mpi_procs = number_of_nodes*32
+number_of_nodes     = [1,1,1,1,1,1]
+number_of_mpi_procs = np.array([1,2,4,8,16,32])
 number_of_threads   = 1
 
 timmings_bi_assembly   = np.zeros((len(problems), max(len(mapping) for mapping in mappings), len(ncells),len(degrees), len(number_of_mpi_procs)))
@@ -43,8 +43,9 @@ for i1,p in enumerate(problems):
 
 #########################################################################################################
 
-number_of_mpi_procs = number_of_nodes*2
-number_of_threads = [16]*len(number_of_nodes)
+number_of_nodes = [1,1,1,1,1,1]
+number_of_mpi_procs = np.array([1,1,1,1,1,1])
+number_of_threads = [1,2,4,8,16,32]
 
 timmings_bi_assembly_mth  = np.zeros((len(problems), max(len(mapping) for mapping in mappings), len(ncells),len(degrees), len(number_of_mpi_procs)))
 timmings_dot_p_mth        = np.zeros((len(problems), max(len(mapping) for mapping in mappings), len(ncells),len(degrees), len(number_of_mpi_procs)))
@@ -75,16 +76,16 @@ for i1,p in enumerate(problems):
                 scaling_bi_assembly_mth[i1,i2,i3,i4,:] = timmings_bi_assembly_mth[i1,i2,i3,i4,k[0]]/timmings_bi_assembly_mth[i1,i2,i3,i4,:]/nn
                 scaling_dot_p_mth[i1,i2,i3,i4,:]       = timmings_dot_p_mth[i1,i2,i3,i4,k[0]]/timmings_dot_p_mth[i1,i2,i3,i4,:]/nn
 
-##########################################################################################################
+#########################################################################################################
 from tabulate import tabulate
-headers = [""] + [str(nn) for nn in number_of_nodes]
+headers = [""] + [str(np*nt) for np,nt in zip(number_of_nodes, number_of_threads)]
 
 for i1,p in enumerate(problems):
     for i2,mapping in enumerate(mappings[i1]):
         if all(np.isnan(v) for v in timmings_bi_assembly[i1,i2].flatten()):continue
         mapping = ('{} analytical mapping' if mapping[1] else '{} Nurbs mapping').format(mapping[0])
         print("="*45,"Timings of the Matrix Assembly of {} with the {}".format(p,mapping), "="*45)
-        T = np.around(timmings_bi_assembly_mth[i1,i2], decimals=5)
+        T = np.around(timmings_bi_assembly[i1,i2], decimals=5)
         newT = []
         for i2,nc in enumerate(ncells):
             for i3,d in enumerate(degrees):
@@ -108,27 +109,27 @@ line_styles = ['>-','o-','s-','v-']
 from itertools import product
 
 titles = ['Matrix Assembly', 'Matrix Vector Product','Matrix Assembly', 'Matrix Vector Product']
-fnames = ['matrix_assembly', 'matrix_vector_product','matrix_assembly_mth', 'matrix_vector_product_mth']
-xaxist = [r'number of nodes', r'number of nodes',r'number of nodes',r'number of nodes']
+fnames  = ['matrix_assembly_sn', 'matrix_vector_product_sn','matrix_assembly_sn_mth', 'matrix_vector_product_sn_mth']
+xaxist = [r'number of mpi procs', r'number of mpi procs',r'number of threads', r'number of threads']
 timings = [timmings_bi_assembly[0,0], timmings_dot_p[0,0],timmings_bi_assembly_mth[0,0], timmings_dot_p_mth[0,0]]
-number_of_nodes = np.array([1,2,4,8,16,32,64])
+number_of_mpi_procs = np.array([1,2,4,8,16,32])
 
 for title,fname,timings_i,xlabel in zip(titles, fnames, timings,xaxist):
     fig = plt.figure(figsize=(10,15))
     ax = fig.add_subplot(1, 1, 1)
-    ax.plot(number_of_nodes,[5*np.nanmax(timings_i)/2**d for d in range(len(number_of_mpi_procs))], color='black', linestyle='dashed', label='perfect scaling')
+    ax.plot(number_of_mpi_procs,[5*np.nanmax(timings_i)/2**d for d in range(len(number_of_mpi_procs))], color='black', linestyle='dashed', label='perfect scaling')
     for nc in range(len(ncells)):
         for p in range(degrees[0],degrees[-1]+1):
 
             mask = np.isfinite(timings_i[nc,p-degrees[0]])
-            line, = ax.plot(number_of_nodes[mask], timings_i[nc,p-degrees[0]][mask], line_styles[nc],color=colors[p-degrees[0]])
+            line, = ax.plot(number_of_mpi_procs[mask], timings_i[nc,p-degrees[0]][mask], line_styles[nc],color=colors[p-degrees[0]])
 
         row = '$n_{{el}}={}^3$'.format(ncells[nc])
-        line, = ax.plot(np.nan*number_of_nodes[mask], np.nan*timings_i[nc,degrees[0]][mask], line_styles[nc],color='k', label=row)
+        line, = ax.plot(np.nan*number_of_mpi_procs[mask], np.nan*timings_i[nc,degrees[0]][mask], line_styles[nc],color='k', label=row)
 
     for p in range(degrees[0],degrees[-1]+1):
         row = '$p={}$'.format(p)
-        line, = ax.plot(np.nan*number_of_nodes[mask], np.nan*timings_i[0,degrees[0]][mask],color=colors[p-degrees[0]], label=row)
+        line, = ax.plot(np.nan*number_of_mpi_procs[mask], np.nan*timings_i[0,degrees[0]][mask],color=colors[p-degrees[0]], label=row)
 
 
     box = ax.get_position()
@@ -136,7 +137,7 @@ for title,fname,timings_i,xlabel in zip(titles, fnames, timings,xaxist):
 
     # Put a legend to the right of the current axis
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    ax.set_xlabel( r'number of nodes', rotation='horizontal' )
+    ax.set_xlabel( xlabel, rotation='horizontal' )
     ax.set_ylabel( r'time [s]' )
     ax.set_xscale('log')
     ax.set_yscale('log')
